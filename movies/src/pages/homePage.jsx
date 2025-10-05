4f6893863a24161293b3fc26feffc168eed8a246
Rayan <20107383@setu.ie>
2025-10-05 21:35:36 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -25,6 +25,13 @@ const HomePage = (props) => {
     else setGenreFilter(value);
   };
 
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -52,8 +59,8 @@ const HomePage = (props) => {
       genreFilter={genreFilter}
     />
 
-        </Grid>
-          <MovieList movies={displayedMovies} />
+        </Grid>    
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
